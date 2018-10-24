c61fd5b67689b222b6aa047d3a8ddca5dd275927
genesisb17 <genesis.bonds@gmail.com>
2018-10-24 16:44:56 -0400

    removing all bad files

--- Week 1/examples/JavaFundamentals/src/com/revature/intro/HelloWorld.java
+++
@@ -1,9 +0,0 @@
-package com.revature.intro; //first line of any Java class
-
-public class HelloWorld {
-	
-	public static void main(String[] args) {
-		System.out.println("First arg:" + args[0]);
-	}
-
-}
