2cbebc6a79d10533a17263d6e7588b6c5a5391c6
Ted Airey <ted.airey4@gmail.com>
2018-10-27 19:14:21 -0400

    Week 1 Folder

---
+++ Week 1/examples/JavaFundamentals/src/com/revature/intro/HelloWorld.java
@@ -0,0 +1,9 @@
+package com.revature.intro; //first line of any Java class
+
+public class HelloWorld {
+	
+	public static void main(String[] args) {
+		System.out.println("First arg:" + args[0]);
+	}
+
+}
