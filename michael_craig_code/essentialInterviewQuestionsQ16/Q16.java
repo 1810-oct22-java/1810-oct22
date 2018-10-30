8b65e6ca6e21a7ac0165b361aa9c8e031058e4e8
Michael Craig <michaelcraig@Michaels-MacBook-Pro.local>
2018-10-29 22:25:06 -0400

    q16 added

---
+++ michael_craig_code/essentialInterviewQuestionsQ16/Q16.java
@@ -0,0 +1,9 @@
+package essentialInterviewQuestionsQ16;
+
+public class Q16 {
+
+	public static void main(String[] args) {
+		System.out.println("The command line arguments are: " + args[0] + " " + args[1]);
+		System.out.println("And the length is: " + args.length);
+	}
+}
