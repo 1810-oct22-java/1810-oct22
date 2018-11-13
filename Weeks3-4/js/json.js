f67ad6f49d4b279b27cad4c2dc36d8492971b6a9
groman-gonzalez <gerald.romangonzalez@gmail.com>
2018-11-13 15:46:16 -0500

    newcode

--- Weeks3-4/js/json.js
+++ Weeks3-4/js/json.js
@@ -1,28 +1,12 @@
-/*
-JSON - JavaScript Object Notation is a minimal, readble format for 
-structuring data. It is used primarily to transmit data between 
-server and client, as an alternative to XML
-
-"lightweight data interchange format modeled after JS objects"
-
-it is NOT a JS object
-*/
 let person = {
-    name: 'GB', 
-    workplace: 'Revature',
-    skills: ['coding', 'talking to people', 'eating']
+    name: 'GB',
+    worksplace: "Revature",
+    skils: ['coding','talking to people','eating']
 }
-
 let personStr = JSON.stringify(person);
 
-//"{"name":"GB","workplace":"Revature","skills":["coding","talking to people","eating"]}";
-
-///////////
-
-let respString = ""; //fulled with some text
-let resp = JSON.parse(respString);
-
+/////
+let repString
+let resp = JSON.parse(repString);
 resp.field1;
 resp.field2;
-
-
