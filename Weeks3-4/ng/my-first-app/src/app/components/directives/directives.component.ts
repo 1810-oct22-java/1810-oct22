38122283bd9c4207be1c2d26ed0570dbf262e562
genesisb17 <genesis.bonds@gmail.com>
2018-11-19 16:05:54 -0500

    pipes

--- Weeks3-4/ng/my-first-app/src/app/components/directives/directives.component.ts
+++ Weeks3-4/ng/my-first-app/src/app/components/directives/directives.component.ts
@@ -9,6 +9,52 @@ export class DirectivesComponent implements OnInit {
 
   showIf = true;
   numArr = [1, 5, 6, 7, 8, 9];
+  employees = [
+    {
+      name: 'Gen',
+      role: 'Trainer'
+    },
+    {
+      name: 'Peter',
+      role: 'Trainer'
+    },
+    {
+      name: 'Carolyn',
+      role: 'Trainer'
+    },
+    {
+      name: 'Quinn',
+      role: 'Trainer'
+    },
+    {
+      name: 'Nick',
+      role: 'Trainer'
+    },
+    {
+      name: 'Richard',
+      role: 'Trainer'
+    },
+    {
+      name: 'Phil',
+      role: 'Associate'
+    },
+    {
+      name: 'Margy',
+      role: 'Associate'
+    },
+    {
+      name: 'James',
+      role: 'Associate'
+    },
+    {
+      name: 'Michael',
+      role: 'Associate'
+    },
+    {
+      name: 'Bill',
+      role: 'Associate'
+    }
+  ];
 
   constructor() { }
 
