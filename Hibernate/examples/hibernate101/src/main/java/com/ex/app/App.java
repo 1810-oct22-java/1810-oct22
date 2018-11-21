143cc9c886288e6937be5a753ab061fb5f808054
genesisb17 <genesis.bonds@gmail.com>
2018-11-21 11:52:06 -0500

    criteria and query

--- Hibernate/examples/hibernate101/src/main/java/com/ex/app/App.java
+++ Hibernate/examples/hibernate101/src/main/java/com/ex/app/App.java
@@ -6,15 +6,31 @@ import com.ex.models.User;
 public class App {
 
 	public static void main(String[] args) {
-		UserDao dao = new UserDao();
+	UserDao dao = new UserDao();
+//		SAVING USERS
+	
+//		User u1 = new User("username", "password", "firstname", "lastname");
+//		User u2 = new User("gb123", "123", "Gen", "Bonds");
+//		User u3 = new User("testuser", "abc", "Test", "User");
+//		
+//		dao.save(u1);
+//		dao.save(u2);
+//		dao.save(u3);
+	
+// 		RETRIEVING USERS
+//		System.out.println("Retrieving user of id 50 with GET");
+//		User u50 = dao.getById(50);
+//		System.out.println("ABOUT TO TO STRING WITH GET");
+//		System.out.println(u50.toString());
+//
+//		
+//		System.out.println("Retrieving user of id 7 with LOAD");
+//		User u50load = dao.loadById(7);
 		
-		User u1 = new User("username", "password", "firstname", "lastname");
-		User u2 = new User("gb123", "123", "Gen", "Bonds");
-		User u3 = new User("testuser", "abc", "Test", "User");
-		
-		dao.save(u1);
-		dao.save(u2);
-		dao.save(u3);
+	User u = dao.findByUsername("testuser");
+	System.out.println(u);
+	
+	
 	}
 
 }
