a2c249f242a75bd857278d8275e2efa76eb4274c
Michael Craig <michaelcraig@Michaels-MacBook-Pro.local>
2018-11-23 12:07:48 -0500

    manager and emp reimbursement tables fully functional

--- Weeks3-4/Project1/src/main/java/com/p1/servlets/EmployeeServlet.java
+++ Weeks3-4/Project1/src/main/java/com/p1/servlets/EmployeeServlet.java
@@ -19,45 +19,51 @@ import com.p1.pojos.Reimbursement;
 import com.p1.service.ReimbursementService;
 
 @WebServlet("/employee")
-public class EmployeeServlet extends HttpServlet{
+public class EmployeeServlet extends HttpServlet {
 
 	private static Logger logger = Logger.getLogger(EmployeeServlet.class);
 
 	static ReimbursementService rService = new ReimbursementService();
-	
+
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		List<Reimbursement> rs = rService.getAllRmbs();
-//		List<Reimbursement> empRs = new ArrayList<Reimbursement>();
-		System.out.println(rs);
 		HttpSession session = req.getSession();
 		String[] s = session.getAttribute("user").toString().split(", ");
-		logger.trace("s: " + s);
 		String userId = null;
+		String roleId = null;
+		logger.trace(s);
 		for (String x : s) {
 			if (x.contains("users_id")) {
 				userId = x.substring(16);
-				logger.trace("userId " + userId);
+				logger.trace("userId: " + userId);
+			} else if (x.contains("role_id")) {
+				logger.trace("x for role id: " + x);
+				roleId = x.substring(8, 9);
+				logger.trace("set roleId: " + roleId);
+			} else {
+				continue;
 			}
 		}
-		for (int i = 0; i < rs.size(); i++) {
-			logger.trace("author=" + userId);
-			logger.trace("success? : " + rs.get(i).toString().contains("author=" + userId));
-			logger.trace(rs.size());
-			if (!rs.get(i).toString().contains("author=" + userId)) {
-				logger.trace("adding for: " + userId);
-				rs.remove(rs.get(i));
-				logger.trace("rs status: " + rs);
+		if (roleId.equals("1") || roleId.equals("1]")) {
+			for (int i = 0; i < rs.size(); i++) {
+				logger.trace("roleId: " + roleId);
+				logger.trace("if false, remove: " + rs.get(i).toString().contains("author=" + userId));
+				logger.trace(rs.size());
+				if (!rs.get(i).toString().contains("author=" + userId)) {
+					logger.trace("removing");
+					rs.remove(rs.get(i));
+				}
+				logger.trace("rs length: " + rs.size());
 			}
-			logger.trace(rs.size());
+			logger.trace("end, roleId: " + roleId);
 		}
-		logger.trace("rs: " + rs.size());
 		ObjectMapper mapper = new ObjectMapper();
 		String json = mapper.writeValueAsString(rs);
 		logger.trace("FINDING EMP R. JSON: " + json);
-		PrintWriter writer = resp.getWriter();		
+		PrintWriter writer = resp.getWriter();
 		resp.setContentType("application/json");
 		writer.write(json);
 	}
-	
+
 }
