d61a5ff8b5e47011e6d2a859f7a7299707739d60
genesisb17 <genesis.bonds@gmail.com>
2018-11-26 09:03:23 -0500

    pushing up from long weekend

--- Hibernate/examples/hibernate101/src/main/java/com/ex/app/App.java
+++ Hibernate/examples/hibernate101/src/main/java/com/ex/app/App.java
@@ -2,42 +2,70 @@ package com.ex.app;
 
 import java.util.List;
 
+import com.ex.dao.PostDao;
 import com.ex.dao.UserDao;
+import com.ex.models.Post;
 import com.ex.models.User;
 
 public class App {
 
+	static UserDao dao = new UserDao();
+	static PostDao postDao = new PostDao();
+	
 	public static void main(String[] args) {
-	UserDao dao = new UserDao();
-//		SAVING USERS
-	
-//		User u1 = new User("username", "password", "firstname", "lastname");
-//		User u2 = new User("gb123", "123", "Gen", "Bonds");
-//		User u3 = new User("testuser", "abc", "Test", "User");
-//		
-//		dao.save(u1);
-//		dao.save(u2);
-//		dao.save(u3);
-	
-// 		RETRIEVING USERS
-//		System.out.println("Retrieving user of id 50 with GET");
-//		User u50 = dao.getById(50);
-//		System.out.println("ABOUT TO TO STRING WITH GET");
-//		System.out.println(u50.toString());
-//
-//		
-//		System.out.println("Retrieving user of id 7 with LOAD");
-//		User u50load = dao.loadById(7);
 		
-	User u = dao.findByUsername("ter");
-	System.out.println(u);
-	
-	System.out.println("FINDING ALL");
-	List<User> users = dao.findAll();
-	for(User user : users) {
-		System.out.println(user);
+		System.out.println("FINDING ALL");
+		List<User> users = dao.findAll();
+		for(User user : users) {
+			System.out.println(user);
+		}
 	}
 	
+	static void testingMultiplicity() {
+		User u = new User("post", "post", "post", "post");
+		Post p = new Post("subject", "body", u);
+		p = postDao.save(p);
+		System.out.println(p);
+	}
+	
+	static void addPostToUser(int userId, String subject, String body) {
+		User u = dao.getById(userId);
+		Post p = new Post(subject, body, u);
+		postDao.save(p);
+		
 	}
 
+	static void testingRead() {
+
+		System.out.println("Retrieving user of id 50 with GET");
+		User u50 = dao.getById(50);
+		System.out.println("ABOUT TO TO STRING WITH GET");
+		System.out.println(u50.toString());
+
+
+		System.out.println("Retrieving user of id 7 with LOAD");
+		User u50load = dao.loadById(7);
+
+		User u = dao.findByUsername("ter");
+		System.out.println(u);
+
+		System.out.println("FINDING ALL");
+		List<User> users = dao.findAll();
+		for(User user : users) {
+			System.out.println(user);
+		}
+	}
+
+	static void testingCreate(){
+
+		User u1 = new User("username", "password", "firstname", "lastname");
+		User u2 = new User("gb123", "123", "Gen", "Bonds");
+		User u3 = new User("testuser", "abc", "Test", "User");
+
+		dao.save(u1);
+		dao.save(u2);
+		dao.save(u3);
+	}
 }
+
+
